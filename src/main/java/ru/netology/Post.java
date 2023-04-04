84c38016b1b44dcd64af973db1580a554f0c6138
Margarita Kosareva <kosareva-r@ya.ru>
2023-04-04 10:05:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String patronymic; // поле для отчества
+    public String surname; // поле для фамилии
+    public String passport; // 4444 № 44444444
+    public String phone; // +7 (999)-999-99-99
+    public boolean subscription; // true
+    public FormDate birthday; // дата рождения, объект класса FormDate
+}
